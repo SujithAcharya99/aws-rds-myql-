068502ee9940ca8846d443d24868f0be7f3a332f
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2021-02-26 09:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,31 +46,31 @@ app.get('/Persons/:id', (req, res) => {
     })
 });
 
-// //Delete an employees
-// app.delete('/employees/:id', (req, res) => {
-//     mysqlConnection.query('DELETE FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
-//         if (!err)
-//             res.send('Deleted successfully.');
-//         else
-//             console.log(err);
-//     })
-// });
+//Delete an Persons
+app.delete('/Persons/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM Persons WHERE PersonID = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully.');
+        else
+            console.log(err);
+    })
+});
 
-// //Insert an employees
-// app.post('/employees', (req, res) => {
-//     let emp = req.body;
-//     var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-//     CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-//     mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
-//         if (!err)
-//             rows.forEach(element => {
-//                 if(element.constructor == Array)
-//                 res.send('Inserted employee id : '+element[0].EmpID);
-//             });
-//         else
-//             console.log(err);
-//     })
-// });
+//Insert an Persons
+app.post('/Persons', (req, res) => {
+    let person = req.body;
+    var sql = "SET @PersonID = ?;SET @LastName = ?;SET @FirstName = ?;SET @Address = ?;SET @City = ?; \
+    CALL PersonsAddOrEdit(@PersonID,@LastName,@FirstName,@Address,@City);";
+    mysqlConnection.query(sql, [person.PersonID, person.LastName, person.FirstName, person.Address, person.City], (err, rows, fields) => {
+        if (!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                res.send('Inserted Person id : '+element[0].PersonID);
+            });
+        else
+            console.log(err);
+    })
+});
 
 // //Update an employees
 // app.put('/employees', (req, res) => {
